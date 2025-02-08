b21a3ac268edff0f5f519f62ef204db6655b7fc6
rocky <rb@dustyfeet.com>
2025-02-08 10:19:26 -0500

    Go over packaging

--- admin-tools/make-dist.sh
+++ admin-tools/make-dist.sh
@@ -19,12 +19,10 @@ cd ..
 source pymathics/natlang/version.py
 echo $__version__
 
-pyversion=3.11
+pyversion=3.12
 if ! pyenv local $pyversion ; then
     exit $?
 fi
 
-python setup.py bdist_wheel --universal
-mv -v dist/${PACKAGE}-${__version__}-{py2.,}py3-none-any.whl
-python ./setup.py sdist
+python -m build
 finish
