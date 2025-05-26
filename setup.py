edc657ca26037b71f90881bfcf9f1e5814fa53b5
rocky <rb@dustyfeet.com>
2025-05-25 21:49:57 -0400

    Revise to use pyproject

--- setup.py
+++ setup.py
@@ -1,40 +1,10 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os
-import os.path as osp
-import platform
 import sys
 
-from setuptools import find_namespace_packages, setup
-
-# Ensure user has the correct Python version
-if sys.version_info < (3, 8):
-    print("Mathics support Python 3.8 and above; you have %d.%d" % sys.version_info[:2])
-    sys.exit(-1)
-
-
-def get_srcdir():
-    filename = osp.normcase(osp.dirname(osp.abspath(__file__)))
-    return osp.realpath(filename)
-
-
-def read(*rnames):
-    return open(osp.join(get_srcdir(), *rnames)).read()
-
-
-# Get/set VERSION and long_description from files
-long_description = read("README.rst") + "\n"
-
-__version__ = "0.0.0"  # overwritten by exec below
-
-# stores __version__ in the current namespace
-exec(compile(open("pymathics/natlang/version.py").read(), "version.py", "exec"))
-
-is_PyPy = platform.python_implementation() == "PyPy"
-
-# Install a wordlist.
-# Environment variables "lang", "WORDLIST_SIZE", and "SPACY_DOWNLOAD" override defaults.
+from setuptools import setup
+from setuptools.command.egg_info import egg_info
 
 # Full package name with two-letter language code, e.g. fr, zh
 lang = os.environ.get("lang", "en_core_web_md")
@@ -43,56 +13,23 @@ lang = os.environ.get("lang", "en_core_web_md")
 # sm=small, lg=large, md=medium.
 WORDLIST_SIZE = os.environ.get("WORDLIST_SIZE", "md")
 
-SPACY_DOWNLOAD = os.environ.get("SPACY_DOWNLOAD", "%s" % (lang,))
+SPACY_DOWNLOAD = os.environ.get("SPACY_DOWNLOAD", lang)
+
+
+class CustomCommands(egg_info):
+    """This runs as part of building an sdist"""
+
+    def finalize_options(self):
+        """Run program to create JSON tables"""
+        os.system(f"{sys.executable} -m spacy download {lang}")
+        os.system(f"{sys.executable} -m nltk.downloader 'wordnet2022 omw")
+        super().finalize_options()
+
 
 # FIXME:
 # consider using langid3 and pyenchant
 
 setup(
-    name="pymathics-natlang",
-    version=__version__,
-    packages=find_namespace_packages(include=["pymathics.*"]),
-    install_requires=[
-        "Mathics3>=8.0.0",
-        "click>=8.0",
-        "joblib>=1.0.1",
-        "langid",  # replace with a supported newer package, e.g. via spacy
-        "llvmlite>=0.36",
-        "nltk>=3.8.0",
-        "PatternLite",
-        "pyenchant>=3.2.0",
-        "pycountry>=3.2.0",
-        "spacy>=3.4",
-        "wasabi<1.1.0,>=0.8.2",
-    ],
+    cmdclass={"egg_info": CustomCommands},
     zip_safe=False,
-    maintainer="Mathics3 Group",
-    maintainer_email="rb@dustyfeet.com",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    # metadata for upload to PyPI
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Topic :: Scientific/Engineering :: Physics",
-        "Topic :: Software Development :: Interpreters",
-        "Topic :: Scientific/Engineering :: Human Machine Interfaces",
-        "Topic :: Text Processing",
-        "Topic :: Text Processing :: Linguistic",
-    ],
-    # TODO: could also include long_description, download_url,
 )
-
-os.system("%s -m spacy download %s" % (sys.executable, lang))
-os.system("%s -m nltk.downloader 'wordnet2022 omw" % sys.executable)
